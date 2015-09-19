ff2c41c112fd78d284235845151b5616550bcb4e
LwaziPrusent <lwazi.prusent@gmail.com>
2015-09-19 15:42:05 +0200

    updated password

--- protected/common/init/bootstrap.php
+++ protected/common/init/bootstrap.php
@@ -11,7 +11,7 @@ $conn = array(
     'host'     => '127.0.0.1',
     'dbname'   => 'PACS',
     'user'     => 'root',
-    'password' => ''
+    'password' => 'asaneb17'
 );
 
 $entityManager = EntityManager::create($conn, $config);
